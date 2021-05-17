e1dd19f77b5400fc236a9799bab2c97f8b674080
NOUSINFO\sandeepab <sandeepab@nousinfo.com>
2021-05-17 20:15:35 +0530

    fixed sos testcase

--- qa-automation/src/test/java/at/smartshop/database/columns/CNGmaUser.java
+++ qa-automation/src/test/java/at/smartshop/database/columns/CNGmaUser.java
@@ -6,4 +6,5 @@ public class CNGmaUser {
 	}
 	public static final String PIN_VALUE = "PinValue";
 	public static final String START_BALANCE = "StartBalance";
+	public static final String SHEET_NAME = "SheetName";
 }
--- qa-automation/src/test/java/at/smartshop/sos/tests/Sos.java
+++ qa-automation/src/test/java/at/smartshop/sos/tests/Sos.java
@@ -28,54 +28,109 @@ import at.smartshop.tests.TestInfra;
 @Listeners(at.framework.reportsetup.Listeners.class)
 public class Sos extends TestInfra {
 	private ResultSets dataBase = new ResultSets();
-    private SOSHome sosHome = new SOSHome();
-    private Foundation foundation = new Foundation();
-    private Strings strings = new Strings();
-    private Numbers numbers = new Numbers();
-    private Excel excel = new Excel();
-    private LoadGMA loadGma = new LoadGMA();
-    private NavigationBar navigationBar = new NavigationBar();
-   
-    private Map<String, String> rstProductSummaryData;
-    private Map<String, String> rstLocationListData;
-    private Map<String, String> rstLoadProduct;
-    private Map<String, String> rstGmaUser;
-    private Map<String, String> rstNavigationMenuData;
-    
-	@Test(description= "This test validate the Success message after updaloding the GMA Template")
-    public void verifySuccessMessage() {
-        try {           
-            final String CASE_NUM ="117462";
-            //Reading the Database
-            rstLocationListData = dataBase.getLocationListData(Queries.LOCATION_LIST, CASE_NUM);
-            rstLoadProduct = dataBase.getLoadProductData(Queries.LOAD_PRODUCT, CASE_NUM);
-            rstGmaUser = dataBase.getGmaUserData(Queries.GMA_USER, CASE_NUM);
-            rstProductSummaryData = dataBase.getProductSummaryData(Queries.PRODUCT_SUMMARY, CASE_NUM);
-            rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
-           
-            //Navigating to the Global Market Account page
-            browser.navigateURL(propertyFile.readPropertyFile(Configuration.SOS_CURRENT_URL,FilePath.PROPERTY_CONFIG_FILE));
-            login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER,FilePath.PROPERTY_CONFIG_FILE), propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD,FilePath.PROPERTY_CONFIG_FILE));
-           
-            sosHome.selectOrginazation(propertyFile.readPropertyFile(Configuration.CURRENT_ORG,FilePath.PROPERTY_CONFIG_FILE));
-            navigationBar.navigateToMenuItem(rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM));
-           
-            //Configuring the Fields in GMA page
-            int requiredValue = numbers.generateRandomNumber(0, 999999);
-            String requiredData = strings.getRandomCharacter();
-            String requiredString = (requiredData + Constants.DELIMITER_HASH + requiredData + Constants.DELIMITER_HASH+ requiredData +  Constants.DELIMITER_HASH+ "5" + Constants.DELIMITER_HASH+ requiredData + "@gmail.com" + Constants.DELIMITER_HASH+ String.valueOf(requiredValue) +  Constants.DELIMITER_HASH + requiredData +  Constants.DELIMITER_HASH+ String.valueOf(requiredValue) + Constants.DELIMITER_HASH+ requiredData+"group");
-       
-            excel.writeToExcel(FilePath.GMA_ACCOUNT_TEMPLATE,loadGma.SHEET,rstProductSummaryData.get(CNProductSummary.ITERATION_COUNT), requiredString);
-           
-            loadGma.gMAUser(rstLocationListData.get(CNLocationList.LOCATION_NAME),
-                                            rstGmaUser.get(CNGmaUser.PIN_VALUE),
-                                            rstGmaUser.get(CNGmaUser.START_BALANCE),
-                                            FilePath.GMA_ACCOUNT_TEMPLATE,
-                                            rstLoadProduct.get(CNLoadProduct.DELETE_EXISTING_PRODUCT));       
-            Assert.assertTrue(foundation.isDisplayed(LoadGMA.LBL_SUCCESS));
-           
-        } catch (Exception exc) {
-            Assert.fail(exc.toString());
-        }
-    }
+	private SOSHome sosHome = new SOSHome();
+	private Foundation foundation = new Foundation();
+	private Strings strings = new Strings();
+	private Numbers numbers = new Numbers();
+	private Excel excel = new Excel();
+	private LoadGMA loadGma = new LoadGMA();
+	private NavigationBar navigationBar = new NavigationBar();
+
+	private Map<String, String> rstProductSummaryData;
+	private Map<String, String> rstLocationListData;
+	private Map<String, String> rstLoadProduct;
+	private Map<String, String> rstGmaUser;
+	private Map<String, String> rstNavigationMenuData;
+
+	@Test(description = "This test validate the Success message after updaloding the GMA Template")
+	public void verifySuccessMessage() {
+		try {
+			final String CASE_NUM = "117462";
+			// Reading the Database
+			rstLocationListData = dataBase.getLocationListData(Queries.LOCATION_LIST, CASE_NUM);
+			rstLoadProduct = dataBase.getLoadProductData(Queries.LOAD_PRODUCT, CASE_NUM);
+			rstGmaUser = dataBase.getGmaUserData(Queries.GMA_USER, CASE_NUM);
+			rstProductSummaryData = dataBase.getProductSummaryData(Queries.PRODUCT_SUMMARY, CASE_NUM);
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+
+			// Navigating to the Global Market Account page
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.SOS_CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			sosHome.selectOrginazation(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.navigateToMenuItem(rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM));
+
+			// Configuring the Fields in GMA page
+			int requiredValue = numbers.generateRandomNumber(0, 999999);
+			String requiredData = strings.getRandomCharacter();
+			String requiredString = (requiredData + Constants.DELIMITER_HASH + requiredData + Constants.DELIMITER_HASH
+					+ requiredData + Constants.DELIMITER_HASH + "5" + Constants.DELIMITER_HASH + requiredData
+					+ "@gmail.com" + Constants.DELIMITER_HASH + String.valueOf(requiredValue) + Constants.DELIMITER_HASH
+					+ requiredData + Constants.DELIMITER_HASH + String.valueOf(requiredValue) + Constants.DELIMITER_HASH
+					+ requiredData + "group");
+
+			excel.writeToExcel(FilePath.GMA_ACCOUNT_TEMPLATE, loadGma.SHEET,
+					rstProductSummaryData.get(CNProductSummary.ITERATION_COUNT), requiredString);
+
+			loadGma.gMAUser(rstLocationListData.get(CNLocationList.LOCATION_NAME), rstGmaUser.get(CNGmaUser.PIN_VALUE),
+					rstGmaUser.get(CNGmaUser.START_BALANCE), FilePath.GMA_ACCOUNT_TEMPLATE,
+					rstLoadProduct.get(CNLoadProduct.DELETE_EXISTING_PRODUCT));
+			Assert.assertTrue(foundation.isDisplayed(LoadGMA.LBL_SUCCESS));
+
+		} catch (Exception exc) {
+			Assert.fail(exc.toString());
+		}
+	}
+
+	@Test(description = "Verify whether Payroll ID and Pay Cycle Group are mandatory to be entered in template")
+	public void PayrollIDAndPayCycleUploadNonMandatory() {
+		try {
+			final String CASE_NUM = "117465";
+
+			// Reading test data from DataBase
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+			// rstConsumerSearchData =
+			// db.getConsumerSearchData(dBConnections.CONSUMER_SEARCH, CASE_NUM);
+			rstProductSummaryData = dataBase.getProductSummaryData(Queries.PRODUCT_SUMMARY, CASE_NUM);
+			rstLocationListData = dataBase.getLocationListData(Queries.LOCATION_LIST, CASE_NUM);
+			rstLoadProduct = dataBase.getLoadProductData(Queries.LOAD_PRODUCT, CASE_NUM);
+			rstGmaUser = dataBase.getGmaUserData(Queries.GMA_USER, CASE_NUM);
+
+			// Login into SOS application
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.SOS_CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			// select Organization
+			sosHome.selectOrginazation(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+
+			// click on the Menu
+			foundation.click(SOSHome.MENU);
+
+			// construct string with no value for payroll id and payroll group
+			Assert.assertTrue(foundation.isDisplayed(LoadGMA.BTN_SUBMIT));
+			int requiredValue = numbers.generateRandomNumber(0, 999999);
+			String requiredData = strings.getRandomCharacter();
+			String requiredString = (requiredData + "#" + requiredData + "#" + requiredData + "#" + "5" + "#"
+					+ requiredData + "@gmail.com" + "#" + String.valueOf(requiredValue) + "#" + requiredData + "#" + " "
+					+ "#" + " ");
+
+			// Write excel and upload file
+			excel.writeToExcel(FilePath.GMA_ACCOUNT_TEMPLATE, loadGma.SHEET,
+					rstProductSummaryData.get(CNProductSummary.ITERATION_COUNT), requiredString);			
+			loadGma.gMAUser(rstLocationListData.get(CNLocationList.LOCATION_NAME), rstGmaUser.get(CNGmaUser.PIN_VALUE),
+					rstGmaUser.get(CNGmaUser.START_BALANCE), FilePath.GMA_ACCOUNT_TEMPLATE,
+					rstLoadProduct.get(CNLoadProduct.DELETE_EXISTING_PRODUCT));
+
+			Assert.assertTrue(foundation.isDisplayed(LoadGMA.LBL_SUCCESS));
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			Assert.fail(exc.toString());
+		}
+	}
 }
