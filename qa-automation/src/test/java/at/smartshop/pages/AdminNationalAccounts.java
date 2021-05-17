b935c366c89b5d325a4f5529add5334ca0ff3311
vijaybu <vijaybu@nousinfo.com>
2021-05-17 13:21:13 +0530

    removed blank lines
    removed blank lines

--- qa-automation/src/test/java/at/smartshop/pages/AdminNationalAccounts.java
+++ qa-automation/src/test/java/at/smartshop/pages/AdminNationalAccounts.java
@@ -50,12 +50,10 @@ public class AdminNationalAccounts extends Factory {
 	public static final By SEARCH_BOX = By.cssSelector("input#filterType");
 	public static final By TEXT_RULE_NAME = By.cssSelector("td[aria-describedby='dataGrid_name'] > a");
 	public static final By TEXT_BOX_RULE_NAME = By.cssSelector("input#rulename");
-
 	public static final String COLUMNNATIONALACCOUNTS = "aria-describedby=national-account-grid_nationalAccount";
 	public static final By TXT_FLITER_INPUT = By.xpath("//input[@placeholder='Contains...']");
 	public static final By TXT_PAGE_TITLE = By.xpath("//div[contains(@id,'National Account')]");
 	public static final By BTN_NO = By.xpath("//button[text()='NO']");
-
 	public static final By BTN_NATIONAL_ACCOUNT_CATEGORY = By.id("mng-category");
 	public static final By LBL_LOCATION_TITLE = By.id("corporatetitle");
 
