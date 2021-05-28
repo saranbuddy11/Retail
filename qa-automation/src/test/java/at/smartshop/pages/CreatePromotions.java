05ef77b838cf522f21298ac03e3ff7d1bb885969
dineshreddychinakampalle <68280578+dineshreddychinakampalle@users.noreply.github.com>
2021-05-28 14:50:26 +0530

    Added Test Case 141779

--- qa-automation/src/test/java/at/smartshop/pages/CreatePromotions.java
+++ qa-automation/src/test/java/at/smartshop/pages/CreatePromotions.java
@@ -7,6 +7,7 @@ import at.framework.browser.Factory;
 import at.framework.files.PropertyFile;
 import at.framework.ui.Dropdown;
 import at.framework.ui.Foundation;
+import at.framework.ui.Table;
 import at.framework.ui.TextBox;
 import at.smartshop.keys.Configuration;
 import at.smartshop.keys.FilePath;
@@ -17,15 +18,22 @@ public class CreatePromotions extends Factory {
 	private Dropdown dropdown = new Dropdown();
 	private TextBox textbox = new TextBox();
 	private PropertyFile propertyFile = new PropertyFile();
+	private Table table = new Table();
 
-	private static final By DPD_PROMO_TYPE = By.id("promotype");
-	private static final By TXT_PROMO_NAME = By.id("name");
+	public static final By DPD_PROMO_TYPE = By.id("promotype");
+	public static final By TXT_PROMO_NAME = By.id("name");
 	private static final By TXT_DISPLAY_NAME = By.id("displayname");
-	private static final By BTN_NEXT = By.id("submitBtn");
+	public static final By BTN_NEXT = By.id("submitBtn");
 
 	public static final By DPD_LOCATION = By.id("location-select");
-	private static final By DPD_ORG = By.xpath("//input[@placeholder='Select Org(s) to include']");
-	private static final By DPD_LOC = By.xpath("//input[@placeholder='Select Location(s) to include']");
+	public static final By DPD_ORG = By.xpath("//input[@placeholder='Select Org(s) to include']");
+	public static final By DPD_LOC = By.xpath("//input[@placeholder='Select Location(s) to include']");
+	
+	public static final By LBL_CREATE_PROMOTION = By.xpath("//li[text()='Create Promotion']");
+	public static final By BTN_END_PROMO =By.id("disablepromotion");
+	public static final By BTN_EXPIRE =By.xpath("//button[@class='ajs-button ajs-ok']");
+	public static final By MULTI_SELECT_TENDER_TYPES =By.id("tendertypes");
+	public static final By BTN_OK = By.xpath("//button[text()='OK']");
 
 	public void newPromotion(String PromoType, String PromoName, String DisplayName) {
 
@@ -41,5 +49,17 @@ public class CreatePromotions extends Factory {
 		getDriver().findElement(DPD_LOC).sendKeys(Keys.ENTER);
 
 	}
+	
+	public void expirePromotion(String dataGridname,String promoName){
+		
+		foundation.waitforElement(PromotionList.TXT_SEARCH_PROMONAME, 2000);
+		textbox.enterText(PromotionList.TXT_SEARCH_PROMONAME, promoName);
+		foundation.click(PromotionList.BTN_SEARCH);
+		table.selectRow(dataGridname,promoName);
+		foundation.doubleClick(By.xpath("//td[@aria-describedby='" + dataGridname + "'][text()='" + promoName + "']"));
+		foundation.waitforElement(BTN_END_PROMO, 2000);
+		foundation.click(BTN_END_PROMO);
+		foundation.click(BTN_EXPIRE);
+		}
 
 }
