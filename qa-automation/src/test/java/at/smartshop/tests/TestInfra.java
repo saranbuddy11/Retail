c77e798a8a5bc739d781d360b5bf92cac22355da
dineshreddychinakampalle <68280578+dineshreddychinakampalle@users.noreply.github.com>
2021-06-04 12:53:35 +0530

    Added Config and DB files

--- qa-automation/src/test/java/at/smartshop/tests/TestInfra.java
+++ qa-automation/src/test/java/at/smartshop/tests/TestInfra.java
@@ -8,12 +8,11 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
 
 import at.framework.browser.Browser;
 import at.framework.database.mssql.ResultSets;
 import at.framework.files.PropertyFile;
-import at.smartshop.keys.Configuration;
-import at.smartshop.keys.FilePath;
 import at.smartshop.pages.Login;
 
 @Listeners(at.framework.reportsetup.Listeners.class)
@@ -27,16 +26,16 @@ public class TestInfra {
 		ResultSets.getConnection();
 	}
 	
-	
-	@BeforeMethod	
-	public void beforeMethod() {
-		try {			
-			browser.launch(propertyFile.readPropertyFile(Configuration.DRIVER, FilePath.PROPERTY_CONFIG_FILE),propertyFile.readPropertyFile(Configuration.BROWSER, FilePath.PROPERTY_CONFIG_FILE));
-			//browser.launch(Constants.LOCAL,Constants.CHROME);
-		} catch (Exception exc) {
-			Assert.fail(exc.toString());
-		}
-	}
+
+	@Parameters({"driver", "browser"})
+    @BeforeMethod  
+    public void beforeMethod(String drivers, String browsers) {          
+        try {          
+            browser.launch(drivers,browsers);
+        } catch (Exception exc) {
+            Assert.fail(exc.toString());
+        }
+    }
 
 	@AfterMethod
 	public void afterMethod() {
--- qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
+++ qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
@@ -3,7 +3,6 @@ package at.smartshop.v5.tests;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
 import at.framework.ui.Foundation;
 import at.smartshop.keys.Configuration;
 import at.smartshop.keys.FilePath;
@@ -12,28 +11,29 @@ import at.smartshop.v5.pages.AccountLogin;
 import at.smartshop.v5.pages.AdminMenu;
 import at.smartshop.v5.pages.LandingPage;
 
+
 @Listeners(at.framework.reportsetup.Listeners.class)
 public class V5Test extends TestInfra {
 		
 	private Foundation foundation=new Foundation();
 	private AccountLogin accountLogin = new AccountLogin();
+
 	
 	@Test(description = "C141867 - This test validates the Driver Login and Log Out")
 	public void verifyDriverLoginLogout() {
 		try {
-			final String CASE_NUM = "141867";
-					
-					browser.navigateURL(propertyFile.readPropertyFile(Configuration.V5_APP_URL , FilePath.PROPERTY_CONFIG_FILE));
-					foundation.click(LandingPage.LINK_ENGLISH);
-					foundation.doubleClick(LandingPage.IMG_LOGO);
-					foundation.doubleClick(LandingPage.IMG_LOGO);
-					foundation.click(LandingPage.IMG_LOGO);
-					accountLogin.enterPin("1111");
-					foundation.click(AdminMenu.BTN_SIGN_IN);
-					foundation.isDisplayed(AdminMenu.LINK_DRIVER_LOGOUT);
-					foundation.click(AdminMenu.LINK_DRIVER_LOGOUT);					
-			
-		}catch(Exception exc) {
+			browser.navigateURL(propertyFile.readPropertyFile(Configuration.V5_APP_URL, FilePath.PROPERTY_CONFIG_FILE));
+			foundation.click(LandingPage.LINK_ENGLISH);
+			foundation.doubleClick(LandingPage.IMG_LOGO);
+			foundation.doubleClick(LandingPage.IMG_LOGO);
+			foundation.click(LandingPage.IMG_LOGO);
+			String pin = propertyFile.readPropertyFile(Configuration.V5_DRIVER_PIN, FilePath.PROPERTY_CONFIG_FILE);
+			accountLogin.enterPin(pin);
+			foundation.click(AdminMenu.BTN_SIGN_IN);
+			foundation.isDisplayed(AdminMenu.LINK_DRIVER_LOGOUT);
+			foundation.click(AdminMenu.LINK_DRIVER_LOGOUT);
+
+		} catch (Exception exc) {
 			Assert.fail(exc.toString());
 		}
 	}
