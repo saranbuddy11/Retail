beb9f867532f0808f72ec8f0eff0ce6bb9653b76
vijaybu <vijaybu@nousinfo.com>
2021-06-07 12:07:40 +0530

    updated Account login function
    updated Account login function

--- qa-automation/src/test/java/at/smartshop/v5/pages/AccountLogin.java
+++ qa-automation/src/test/java/at/smartshop/v5/pages/AccountLogin.java
@@ -7,6 +7,7 @@ import at.framework.ui.TextBox;
 
 public class AccountLogin {
 	private Foundation foundation=new Foundation();
+	private TextBox textBox=new TextBox();
 
 public static final By BTN_EMAIL_LOGIN= By.id("email-account-login-btn-id");
 public static final By TXT_EMAIL= By.id("emailLoginInput");
@@ -18,29 +19,12 @@ public static final By BTN_CAMELCASE=By.xpath("//*[@id='accountModal']//div[text
 public static final By BTN_PIN_NEXT=By.id("pin-back-btn-go-id");
 
 public void login(String emailId, String pin) {
-	enterEmail(emailId);
-	enterPin(pin);
-	foundation.click(BTN_PIN_NEXT);
-}
-
-public void enterEmail(String email) {
 	foundation.click(BTN_CAMELCASE);
-	char[] charArray = email.toCharArray();
-	for (char eachChar : charArray) {
-		By obj=By.xpath("//*[@id='accountModal']//div[text()='"+ eachChar +"']");
-		foundation.click(obj);
-	}
-
+	textBox.enterKeypadText(emailId);
 	foundation.click(BTN_ENTER);
-	foundation.click(BTN_NEXT);
-}
-
-public void enterPin(String pin) {
-	for (int i = 0; i < pin.length(); i++) {
-		int number = Integer.parseInt(pin.substring(i, i + 1));
-		foundation.click(By.xpath("//td[text()='"+ number+"']"));
-	}
-	
+    foundation.click(BTN_NEXT);
+    textBox.enterPin(pin);
+	foundation.click(BTN_PIN_NEXT);
 }
 
 }
