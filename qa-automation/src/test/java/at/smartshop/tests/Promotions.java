219b2325042c8985483cd46ffd5af709c76d2781
NOUSINFO\sandeepab <sandeepab@nousinfo.com>
2021-06-08 17:03:24 +0530

    Update Promotions.java

--- qa-automation/src/test/java/at/smartshop/tests/Promotions.java
+++ qa-automation/src/test/java/at/smartshop/tests/Promotions.java
@@ -260,8 +260,9 @@ public class Promotions extends TestInfra {
             Assert.assertEquals(locationName.get(2), locName);
 
 			// Resetting the data
-			promotionList.searchPromotion(promotionName);
-			createPromotions.expirePromotion(gridName, promotionName);
+            promotionList.searchPromotion(promotionName);
+            assertTrue(foundation.getText(PromotionList.TBL_COLUMN_NAME).equals(promotionName));
+            editPromotion.expirePromotion(gridName,promotionName);
 
 		} catch (Exception exc) {
 			Assert.fail(exc.toString());
