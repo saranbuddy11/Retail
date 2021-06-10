80246c6d878a529762cb27ced598c97168b57547
dineshreddychinakampalle <68280578+dineshreddychinakampalle@users.noreply.github.com>
2021-06-10 12:44:00 +0530

    Added Test Case 142666

--- qa-automation/src/test/java/at/smartshop/v5/pages/Order.java
+++ qa-automation/src/test/java/at/smartshop/v5/pages/Order.java
@@ -16,5 +16,6 @@ public class Order {
 	public static final By POP_UP_TIMEOUT_YES = By.id("time-out-btn-yes-id");
 	public static final By POP_UP_TIMEOUT_NO=By.id("time-out-btn-no-id");
 	public static final By LBL_YOUR_ORDER = By.xpath("//h1[text()='Your Order']");
+	public static final By POP_UP_LBL_ORDER_TIMEOUT = By.xpath("//h1[text()='Order Timeout']");
 }
 
--- qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
+++ qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
@@ -146,4 +146,28 @@ public class V5Test extends TestInfra {
 		}
 	}
 	
+	@Test(description = "C142666 - This test validates the Order Time out prompt will disapper after time out")
+	public void verifyPromptAfterTimeOut() {
+		try {
+			
+			final String CASE_NUM = "142666";
+			browser.navigateURL(propertyFile.readPropertyFile(Configuration.V5_APP_URL, FilePath.PROPERTY_CONFIG_FILE));	
+			rstV5DeviceData = dataBase.getV5DeviceData(Queries.V5Device, CASE_NUM);
+			String language = rstV5DeviceData.get(CNV5Device.REQUIRED_DATA);
+			foundation.click(landingPage.objLanguage(language));
+			foundation.click(LandingPage.IMG_SEARCH_ICON);
+			textBox.enterKeypadText(rstV5DeviceData.get(CNV5Device.PRODUCT_NAME));
+			foundation.click(ProductSearch.BTN_PRODUCT);
+			Assert.assertTrue(foundation.isDisplayed(Order.BTN_CANCEL_ORDER));
+			foundation.threadWait(20000);
+			Assert.assertTrue(foundation.isDisplayed(Order.POP_UP_LBL_ORDER_TIMEOUT));
+			foundation.threadWait(20000);
+			Assert.assertTrue(foundation.isDisplayed(LandingPage.IMG_SEARCH_ICON));
+			
+		}catch(Exception exc) {
+			Assert.fail(exc.toString());
+		}
+		
+	}
+	
 }
