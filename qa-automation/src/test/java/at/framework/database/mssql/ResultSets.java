ebefea229be50309b5fb7f06d624c387c5c20684
dineshreddychinakampalle <68280578+dineshreddychinakampalle@users.noreply.github.com>
2021-06-14 18:27:29 +0530

    Updated the Test Case

--- qa-automation/src/test/java/at/framework/database/mssql/ResultSets.java
+++ qa-automation/src/test/java/at/framework/database/mssql/ResultSets.java
@@ -717,6 +717,7 @@ public class ResultSets extends Connections {
 					rstV5Device.put(CNV5Device.ACTUAL_DATA, resultSet.getString(CNV5Device.ACTUAL_DATA));
 					rstV5Device.put(CNV5Device.PIN, resultSet.getString(CNV5Device.PIN));
 					rstV5Device.put(CNV5Device.EMAIL_ID, resultSet.getString(CNV5Device.EMAIL_ID));
+					rstV5Device.put(CNV5Device.TIMEOUT_POPUP, resultSet.getString(CNV5Device.TIMEOUT_POPUP));
 				}
 			}
 		} catch (Exception exc) {
