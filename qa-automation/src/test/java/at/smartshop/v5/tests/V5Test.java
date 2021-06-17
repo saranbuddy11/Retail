7fb186abae82cd28314625ee0b6c0dcce452f488
ajaynous <82958070+ajay8558@users.noreply.github.com>
2021-06-17 14:18:36 +0530

    merged

--- qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
+++ qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
@@ -1,5 +1,6 @@
 package at.smartshop.v5.tests;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.util.Arrays;
@@ -27,51 +28,62 @@ import at.smartshop.v5.pages.UserProfile;
 
 @Listeners(at.framework.reportsetup.Listeners.class)
 public class V5Test extends TestInfra {
-		
-	private Foundation foundation=new Foundation();
-	private AccountLogin accountLogin=new AccountLogin();
-	private ResultSets dataBase = new ResultSets();
-	private LandingPage landingPage=new LandingPage();
-	
-	private Map<String, String> rstV5DeviceData;
-//	@Test(description = "Testing V5 test case execution")
-//	public void verifyLockerSystemsPickupLocation() {
-//		try {
-//			//browser.launch(propertyFile.readPropertyFile(Configuration.DRIVER, FilePath.PROPERTY_CONFIG_FILE),propertyFile.readPropertyFile(Configuration.BROWSER, FilePath.PROPERTY_CONFIG_FILE));
-//			browser.navigateURL(
-//					propertyFile.readPropertyFile(Configuration.V5_APP_URL , FilePath.PROPERTY_CONFIG_FILE));
-//			By createAccount= By.id("create-account-id");
-//			By english= By.xpath("//button[text()='English']");
-//			foundation.click(english);
-//			foundation.click(createAccount);
-//			Thread.sleep(10000);
-//			
-//		} catch (Exception exc) {
-//			Assert.fail(exc.toString());
-//		}
 
-		@Test(description = "C141881 - Kiosk Privacy Policy (if applicable)")
-		public void verifyPrivacyPolicy() {
-			try {
-				final String CASE_NUM = "141881";
-				rstV5DeviceData = dataBase.getV5DeviceData(Queries.V5DEVICE, CASE_NUM);
-		        //List<String> requiredData = Arrays.asList(rstV5DeviceData.get(CNV5Device.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
-				
-		        browser.navigateURL(propertyFile.readPropertyFile(Configuration.V5_APP_URL , FilePath.PROPERTY_CONFIG_FILE));
-		        foundation.click(landingPage.objLanguage("English"));
-		        foundation.click(LandingPage.BTN_LOGIN);
-				foundation.click(AccountLogin.BTN_EMAIL_LOGIN);
-				accountLogin.login(rstV5DeviceData.get(CNV5Device.EMAIL_ID), rstV5DeviceData.get(CNV5Device.PIN));
-	
-				foundation.click(UserProfile.BTN_PRIVACY);
-				String title=foundation.getText(Policy.LBL_POLICY_TITLE);
-				Assert.assertTrue(title.equals(rstV5DeviceData.get(CNV5Device.REQUIRED_DATA)));
-				
-				foundation.click(Policy.BTN_OK);
-				
-			} catch (Exception exc) {
-				Assert.fail(exc.toString());
-			}
+	private Foundation foundation = new Foundation();
+	private AccountLogin accountLogin = new AccountLogin();
+	private ResultSets dataBase = new ResultSets();
+	private LandingPage landingPage = new LandingPage();
+
+	private Map<String, String> rstV5DeviceData;
+
+	@Test(description = "C141881 - Kiosk Privacy Policy (if applicable)")
+	public void verifyPrivacyPolicy() {
+		try {
+			final String CASE_NUM = "141881";
+			rstV5DeviceData = dataBase.getV5DeviceData(Queries.V5DEVICE, CASE_NUM);
+			// List<String> requiredData =
+			// Arrays.asList(rstV5DeviceData.get(CNV5Device.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+
+			browser.navigateURL(propertyFile.readPropertyFile(Configuration.V5_APP_URL, FilePath.PROPERTY_CONFIG_FILE));
+			foundation.click(landingPage.objLanguage("English"));
+			foundation.click(LandingPage.BTN_LOGIN);
+			foundation.click(AccountLogin.BTN_EMAIL_LOGIN);
+			accountLogin.login(rstV5DeviceData.get(CNV5Device.EMAIL_ID), rstV5DeviceData.get(CNV5Device.PIN));
+
+			foundation.click(UserProfile.BTN_PRIVACY);
+			String title = foundation.getText(Policy.LBL_POLICY_TITLE);
+			Assert.assertTrue(title.equals(rstV5DeviceData.get(CNV5Device.REQUIRED_DATA)));
+
+			foundation.click(Policy.BTN_OK);
+
+		} catch (Exception exc) {
+			Assert.fail(exc.toString());
+		}
 	}
 
+	@Test(description = "Kiosk 'Default' Landing UI > Language Selection")
+	public void verifyLanguageSelection() {
+		try {
+			final String CASE_NUM = "141857";
+			rstV5DeviceData = dataBase.getV5DeviceData(Queries.V5DEVICE, CASE_NUM);
+			List<String> actualData = Arrays
+					.asList(rstV5DeviceData.get(CNV5Device.ACTUAL_DATA).split(Constants.DELIMITER_TILD));
+			List<String> requiredData = Arrays
+					.asList(rstV5DeviceData.get(CNV5Device.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+			browser.navigateURL(propertyFile.readPropertyFile(Configuration.V5_APP_URL, FilePath.PROPERTY_CONFIG_FILE));
+
+			foundation.click(landingPage.objLanguage(requiredData.get(0)));
+
+			String actualLanguage = foundation.getText(LandingPage.LBL_HEADER);
+			assertEquals(actualLanguage, actualData.get(0));
+			Thread.sleep(5000);
+
+			foundation.click(landingPage.objLanguage(requiredData.get(1)));
+			actualLanguage = foundation.getText(LandingPage.LBL_HEADER);
+			assertEquals(actualLanguage, actualData.get(1));
+
+		} catch (Exception exc) {
+			Assert.fail();
+		}
+	}
 }
