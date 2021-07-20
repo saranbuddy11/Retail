b96c89a8759442f6b5deda011148f4c8e5770146
vijaybu <vijaybu@nousinfo.com>
2021-07-20 13:42:27 +0530

    updated
    updated

--- qa-automation/src/test/java/at/smartshop/v5/pages/Order.java
+++ qa-automation/src/test/java/at/smartshop/v5/pages/Order.java
@@ -5,12 +5,20 @@ import org.openqa.selenium.By;
 import java.util.Arrays;
 import java.util.List;
 import org.testng.Assert;
+
+import at.framework.files.PropertyFile;
 import at.framework.ui.Foundation;
+import at.framework.ui.TextBox;
+import at.smartshop.database.columns.CNV5Device;
+import at.smartshop.keys.Configuration;
 import at.smartshop.keys.Constants;
+import at.smartshop.keys.FilePath;
 
 public class Order {
 	
 	 private Foundation foundation=new Foundation();
+	 private TextBox textBox=new TextBox();
+	 private PropertyFile propertyFile= new PropertyFile();
 
 	public static final By BTN_CANCEL_ORDER = By.xpath("//button[text()='Cancel Order']");
 	public static final By LBL_ORDER_CANCELLED = By.xpath("//span[text()='Transaction Cancelled']");
@@ -50,4 +58,20 @@ public class Order {
 	    Assert.assertTrue(foundation.isDisplayed(objText(orderPageData.get(7))));
 	    Assert.assertTrue(foundation.isDisplayed(objText(orderPageData.get(8))));
 	}	
+	
+	public void completeOrder(String email,String purchaseComplete, String yesButton) {
+		foundation.click(objText(email));
+		foundation.waitforElement(AccountLogin.BTN_NEXT, Constants.SHORT_TIME);
+		foundation.click(AccountLogin.BTN_CAMELCASE);
+		textBox.enterKeypadText(
+				propertyFile.readPropertyFile(Configuration.V5_USER, FilePath.PROPERTY_CONFIG_FILE));
+		foundation.click(AccountLogin.BTN_NEXT);
+		foundation.waitforElement(AccountLogin.BTN_PIN_NEXT, Constants.SHORT_TIME);
+		textBox.enterPin(propertyFile.readPropertyFile(Configuration.V5_PIN, FilePath.PROPERTY_CONFIG_FILE));
+		foundation.click(AccountLogin.BTN_PIN_NEXT);
+		Assert.assertTrue(foundation.isDisplayed(objText(purchaseComplete)));
+
+		foundation.click(objText(yesButton));
+		foundation.waitforElement(LandingPage.IMG_SEARCH_ICON, Constants.SHORT_TIME);
+	}
 }
