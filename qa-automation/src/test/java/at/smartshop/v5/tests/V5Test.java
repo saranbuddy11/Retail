d5e21ade392a9fefa3f6d28fb6333d1a923b8bb6
vijaybu <vijaybu@nousinfo.com>
2021-07-20 11:29:20 +0530

    Test case completed
    Test case completed

--- qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
+++ qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
@@ -1,9 +1,8 @@
 package at.smartshop.v5.tests;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotEquals;
-import static org.testng.Assert.assertNotSame;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.util.Arrays;
