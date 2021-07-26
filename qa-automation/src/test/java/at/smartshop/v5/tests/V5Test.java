c8c7b3c21a3cfa103634755060ac6f4bf0722782
NOUSINFO\sandeepab <sandeepab@nousinfo.com>
2021-07-26 15:09:51 +0530

    pushing 143123

--- qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
+++ qa-automation/src/test/java/at/smartshop/v5/tests/V5Test.java
@@ -4890,4 +4890,64 @@ public class V5Test extends TestInfra {
 		}
 	}
 	
+	@Test(description = "143123-Add new tax category and Edit it's name and verify edits applied to product or not on product summary page")
+	public void addEditTaxCategory() {
+		try {
+			final String CASE_NUM = "143123";
+			
+			// Reading test data from DataBase
+			rstV5DeviceData = dataBase.getV5DeviceData(Queries.V5Device, CASE_NUM);
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+			String productName=rstV5DeviceData.get(CNV5Device.PRODUCT_NAME);			
+			List<String> requiredData = Arrays
+					.asList(rstV5DeviceData.get(CNV5Device.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+			List<String> menuItem = Arrays
+					.asList(rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM).split(Constants.DELIMITER_TILD));
+
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+
+			//navigate to tax category			
+			navigationBar.navigateToMenuItem(menuItem.get(1));
+			foundation.click(CategoryList.BTN_CREATE_NEW_CATEGORY);
+			
+			//add tax category
+			String newTaxCat = string.getRandomCharacter().toUpperCase();
+			String editedTaxCat=requiredData.get(1)+newTaxCat;
+			categorySummary.addCategory(newTaxCat,requiredData.get(3));
+			
+			//verify newly added category displays in category list page
+			assertTrue(categoryList.verifyCategoryExist(newTaxCat));
+			
+			// Select Menu and Menu Item and add the tax category to a global product
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.navigateToMenuItem(menuItem.get(0));
+			globalProduct.selectGlobalProduct(productName);
+			dropDown.selectItem(ProductSummary.DPD_TAX_CATEGORY, newTaxCat, Constants.TEXT);
+			foundation.click(ProductSummary.BTN_SAVE);
+
+			//edit tax category
+			navigationBar.navigateToMenuItem(menuItem.get(1));
+			categoryList.selectCategory(newTaxCat);
+			categorySummary.updateName(editedTaxCat);
+			navigationBar.navigateToMenuItem(menuItem.get(0));
+			globalProduct.selectGlobalProduct(productName);
+			assertEquals(dropDown.getSelectedItem(ProductSummary.DPD_TAX_CATEGORY), editedTaxCat);
+			
+			//reset data
+			dropDown.selectItem(ProductSummary.DPD_TAX_CATEGORY, requiredData.get(2), Constants.TEXT);
+			foundation.click(ProductSummary.BTN_SAVE);
+			navigationBar.navigateToMenuItem(menuItem.get(1));
+			categoryList.selectCategory(editedTaxCat);
+			categorySummary.updateName(newTaxCat);								
+		} catch (Exception exc) {
+			Assert.fail();
+		}
+	}
+	
 }
