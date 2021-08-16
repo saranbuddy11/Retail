eb02ffa90d02588fc7aa82e08dd3780ba576c90e
ajaynous <82958070+ajay8558@users.noreply.github.com>
2021-08-16 18:23:14 +0530

    completed

--- qa-automation/src/test/java/at/smartshop/tests/Menu.java
+++ qa-automation/src/test/java/at/smartshop/tests/Menu.java
@@ -14,6 +14,7 @@ import at.framework.generic.Strings;
 import at.framework.ui.CheckBox;
 import at.framework.ui.Dropdown;
 import at.framework.ui.Foundation;
+import at.framework.ui.Radio;
 import at.framework.ui.Table;
 import at.framework.ui.TextBox;
 import at.smartshop.database.columns.CNLocation;
@@ -40,6 +41,7 @@ public class Menu extends TestInfra {
 	private LocationSummary locationSummary = new LocationSummary();
 	private SelfService selfService = new SelfService();
 	private CheckBox checkBox = new CheckBox();
+	private Radio radio = new Radio();
 
 	private Map<String, String> rstNavigationMenuData;
 	private Map<String, String> rstLocationData;
@@ -108,8 +110,8 @@ public class Menu extends TestInfra {
 			foundation.waitforElement(SelfService.LBL_HAS_PRINT, Constants.SHORT_TIME);
 			actualData = foundation.getText(SelfService.LBL_HAS_PRINT);
 			Assert.assertEquals(actualData, requiredData.get(2));
-			
-			//deselect printgroup checkbox
+
+			// deselect printgroup checkbox
 			foundation.click(SelfService.LBL_FORWARD_ARROW);
 			checkBox.unCheck(selfService.objPrintCheckbox(printGroupName));
 
@@ -123,7 +125,6 @@ public class Menu extends TestInfra {
 			Assert.assertEquals(actualData, requiredData.get(1));
 			foundation.click(SelfService.BTN_DELETE);
 			foundation.alertAccept();
-			
 
 		} catch (Exception exc) {
 			Assert.fail(exc.toString());
@@ -187,8 +188,8 @@ public class Menu extends TestInfra {
 			foundation.waitforElement(SelfService.LBL_HAS_PRINT, Constants.SHORT_TIME);
 			actualData = foundation.getText(SelfService.LBL_HAS_PRINT);
 			Assert.assertEquals(actualData, requiredData.get(2));
-			
-			//deselect printgroup checkbox
+
+			// deselect printgroup checkbox
 			foundation.click(SelfService.LBL_FORWARD_ARROW);
 			checkBox.unCheck(selfService.objPrintCheckbox(requiredData.get(0)));
 
@@ -200,7 +201,7 @@ public class Menu extends TestInfra {
 			foundation.waitforElement(SelfService.LBL_INHERIT_PRINT, Constants.SHORT_TIME);
 			actualData = foundation.getText(SelfService.LBL_INHERIT_PRINT);
 			Assert.assertEquals(actualData, requiredData.get(1));
-									
+
 			foundation.click(SelfService.BTN_DELETE);
 			foundation.alertAccept();
 			foundation.waitforElement(SelfService.FILTER_MENU, Constants.SHORT_TIME);
@@ -216,6 +217,199 @@ public class Menu extends TestInfra {
 			foundation.click(locationSummary.objPrintGroup(requiredData.get(3)));
 			foundation.click(LocationSummary.BTN_SAVE);
 			foundation.waitforElement(LocationSummary.LBL_SPINNER_MSG, Constants.SHORT_TIME);
+
+		} catch (Exception exc) {
+			Assert.fail(exc.toString());
+		}
+	}
+
+	@Test(description = "145288-QAA-218-Verify Hide feature for All button is working")
+	public void verifyHideButton() {
+		try {
+			final String CASE_NUM = "145288";
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Reading test data from DataBase
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+			rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
+
+			List<String> requiredData = Arrays
+					.asList(rstLocationData.get(CNLocation.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+			String menuItem = rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM);
+
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.navigateToMenuItem(menuItem);
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			radio.set(SelfService.RDO_BTN_Hide);
+			foundation.click(SelfService.BTN_SAVE);
+
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			boolean isenabled = radio.isSelected(SelfService.RDO_BTN_Hide);
+			Assert.assertTrue(isenabled);
+			Assert.assertFalse(foundation.isDisplayed(SelfService.LBL_ALL));
+
+		} catch (Exception exc) {
+			Assert.fail(exc.toString());
+		}
+	}
+
+	@Test(description = "145289-QAA-218- Verify when show radio button is enabled in self service menu, All option is displayed.")
+	public void verifyShowButton() {
+		try {
+			final String CASE_NUM = "145289";
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Reading test data from DataBase
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+			rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
+
+			List<String> requiredData = Arrays
+					.asList(rstLocationData.get(CNLocation.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+			String menuItem = rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM);
+
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.navigateToMenuItem(menuItem);
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			radio.set(SelfService.RDO_BTN_SHOW);
+			foundation.click(SelfService.BTN_SAVE);
+
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			boolean isenabled = radio.isSelected(SelfService.RDO_BTN_SHOW);
+			Assert.assertTrue(isenabled);
+			Assert.assertTrue(foundation.isDisplayed(SelfService.LBL_ALL));
+
+		} catch (Exception exc) {
+			Assert.fail(exc.toString());
+		}
+	}
+
+	@Test(description = "145290-QAA-218-verify when new self service menu is created, show radio button is enabled by default")
+	public void verifyDefualtSettings() {
+		try {
+			final String CASE_NUM = "145290";
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Reading test data from DataBase
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+			rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
+
+		
+			String menuItem = rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM);
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.navigateToMenuItem(menuItem);
+			foundation.click(SelfService.BTN_CREATE_NEW);
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			boolean isenabled = radio.isSelected(SelfService.RDO_BTN_SHOW);
+			Assert.assertTrue(isenabled);
+			Assert.assertTrue(foundation.isDisplayed(SelfService.LBL_ALL));
+			
+		} catch (Exception exc) {
+			Assert.fail(exc.toString());
+		}
+	}
+	@Test(description = "145292-QAA-218-Verify cancel button in self service menu for HIDE Radio Button")
+	public void verifyCancelButtonHide() {
+		try {
+			final String CASE_NUM = "145292";
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Reading test data from DataBase
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+			rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
+
+			List<String> requiredData = Arrays
+					.asList(rstLocationData.get(CNLocation.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+			String menuItem = rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM);
+
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.navigateToMenuItem(menuItem);
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			radio.set(SelfService.RDO_BTN_SHOW);
+			foundation.click(SelfService.BTN_SAVE);
+
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			radio.set(SelfService.RDO_BTN_Hide);
+			foundation.click(SelfService.BTN_CANCEL);
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			boolean isenabled = radio.isSelected(SelfService.RDO_BTN_SHOW);
+			Assert.assertTrue(isenabled);
+			Assert.assertTrue(foundation.isDisplayed(SelfService.LBL_ALL));
+			isenabled = radio.isSelected(SelfService.RDO_BTN_Hide);
+			Assert.assertFalse(isenabled);
+
+		} catch (Exception exc) {
+			Assert.fail(exc.toString());
+		}
+	}
+	@Test(description = "145291-QAA-218-Verify cancel button in self service menu for SHOW Radio Button")
+	public void verifyCancelButtonShow() {
+		try {
+			final String CASE_NUM = "145291";
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Reading test data from DataBase
+			rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+			rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
+
+			List<String> requiredData = Arrays
+					.asList(rstLocationData.get(CNLocation.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+			String menuItem = rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM);
+
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.navigateToMenuItem(menuItem);
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_Hide, Constants.SHORT_TIME);
+			radio.set(SelfService.RDO_BTN_Hide);
+			foundation.click(SelfService.BTN_SAVE);
+
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			radio.set(SelfService.RDO_BTN_SHOW);
+			foundation.click(SelfService.BTN_CANCEL);
+			textBox.enterText(SelfService.FILTER_MENU, requiredData.get(0));
+			table.selectRow(requiredData.get(0));
+			foundation.waitforElement(SelfService.RDO_BTN_SHOW, Constants.SHORT_TIME);
+			boolean isSelected = radio.isSelected(SelfService.RDO_BTN_SHOW);
+			Assert.assertFalse(isSelected);
+			Assert.assertFalse(foundation.isDisplayed(SelfService.LBL_ALL));
+			isSelected = radio.isSelected(SelfService.RDO_BTN_Hide);
+			Assert.assertTrue(isSelected);
 
 		} catch (Exception exc) {
 			Assert.fail(exc.toString());
