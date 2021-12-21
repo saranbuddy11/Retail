c6c37eb98f96f9b72d1d0007e4b2d91522d25959
PrabhaNgm <91895306+PrabhaNgm@users.noreply.github.com>
2021-12-21 20:10:56 +0530

    QAA-304
    Completed

--- qa-automation/src/test/java/at/smartshop/tests/SuperOthers.java
+++ qa-automation/src/test/java/at/smartshop/tests/SuperOthers.java
@@ -21,13 +21,17 @@ import at.framework.ui.Dropdown;
 import at.framework.ui.Foundation;
 import at.framework.ui.Table;
 import at.framework.ui.TextBox;
+import at.smartshop.database.columns.CNDeviceList;
 import at.smartshop.database.columns.CNNavigationMenu;
 import at.smartshop.database.columns.CNOrgSummary;
+import at.smartshop.database.columns.CNSuperList;
 import at.smartshop.keys.Configuration;
 import at.smartshop.keys.Constants;
 import at.smartshop.keys.FilePath;
 import at.smartshop.pages.ContactList;
+import at.smartshop.pages.CorporateAccountList;
 import at.smartshop.pages.CreatePromotions;
+import at.smartshop.pages.DataSourceManager;
 import at.smartshop.pages.EditPromotion;
 import at.smartshop.pages.GlobalProduct;
 import at.smartshop.pages.LocationList;
@@ -48,7 +52,7 @@ public class SuperOthers extends TestInfra {
 	private LocationList locationList = new LocationList();
 	private Dropdown dropdown = new Dropdown();
 	private TextBox textBox = new TextBox();
-	private Strings strings = new Strings();
+	private Strings string = new Strings();
 	private DateAndTime dateAndTime = new DateAndTime();
 	private PromotionList promotionList = new PromotionList();
 	private EditPromotion editPromotion = new EditPromotion();
@@ -63,6 +67,7 @@ public class SuperOthers extends TestInfra {
 	private Map<String, String> rstLocationListData;
 	private Map<String, String> rstLocationSummaryData;
 	private Map<String, String> rstUserRolesData;
+	private Map<String, String> rstSuperListData;
 
 	@Test(description = "164514-ADM>Super>Contact>Contact List")
 	public void superContactList() {
@@ -103,5 +108,48 @@ public class SuperOthers extends TestInfra {
 			foundation.deleteFile(FilePath.EXCEL_CONTACT_SRC);
 		}
 	}
+	
+	@Test(description = "166895-Validate the check and uncheck checkbox of ' Is Snowflake' column")
+	public void DSMCheckUncheckCheckbox() {
+		final String CASE_NUM = "166895";
+		// Reading test data from DataBase
+		rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+		rstSuperListData = dataBase.getSuperListData(Queries.SUPER, CASE_NUM);
+		
+		final String super_Name = rstSuperListData.get(CNSuperList.SUPER_NAME);
+		
+		List<String> rowdata = Arrays
+				.asList(rstSuperListData.get(CNSuperList.DISBURSEMENT_PAGE_RECORD).split(Constants.DELIMITER_TILD));
+		String search_data = rowdata.get(0);
+	
+		try {
 
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Select Menu and Menu Item
+			navigationBar.navigateToMenuItem(rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM));
+			
+			foundation.waitforElement(DataSourceManager.VALIDATE_DSM_HEADING, Constants.SHORT_TIME);
+			assertTrue(foundation.isDisplayed(DataSourceManager.VALIDATE_DSM_HEADING),super_Name);
+			
+			//search for Report 
+			textBox.enterText(DataSourceManager.DSM_SEARCH_BOX, search_data);
+			assertTrue(checkBox.isChkEnabled(DataSourceManager.DSM_CHECKBOX));
+			foundation.click(DataSourceManager.DSM_CHECKBOX);
+			foundation.waitforElement(DataSourceManager.DSM_SUCCESS_POPUP, Constants.SHORT_TIME);
+		
+		} catch (Throwable exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		} finally {
+			assertTrue(checkBox.isChecked(DataSourceManager.DSM_CHECKBOX));
+			foundation.click(DataSourceManager.DSM_CHECKBOX);
+			foundation.waitforElement(DataSourceManager.DSM_SUCCESS_POPUP, Constants.SHORT_TIME);
+
+		}
+	}
 }
