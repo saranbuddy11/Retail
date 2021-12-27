7296ee5b73f91174c3e16fc38ae0e7c105daacbe
NOUSINFO\sandeepab <sandeepab@nousinfo.com>
2021-12-27 20:58:18 +0530

    pushing fixes

--- qa-automation/src/test/java/at/smartshop/keys/Configuration.java
+++ qa-automation/src/test/java/at/smartshop/keys/Configuration.java
@@ -43,7 +43,7 @@ public class Configuration {
 	public static final String EMAIL_TO = "emailTo";
 	public static final String EMAIL_HOST_NAME = "emailHostName";
 	public static final String DRIVER_USER = "DriverUser";
-	public static final String AUTOMATIONLOCATION1 = "second_loc";
+	public static final String AUTOMATIONLOCATION1 = "third_loc";
 	public static final String CONSUMER_USER = "consumerUser";
 	public static final String CONSUMER_PASSWORD = "consumerPassword";
 }
