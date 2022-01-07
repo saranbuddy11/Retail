1574b097ef2b82306eecb66e92f563aafbb52dfa
ravindhara <ravindhara@nousinfo.com>
2022-01-07 04:28:55 -0800

    Un commented Snowflake/RDS Method
    Un commented Snowflake/RDS Method

--- qa-automation/src/test/java/at/smartshop/tests/Report.java
+++ qa-automation/src/test/java/at/smartshop/tests/Report.java
@@ -130,18 +130,18 @@ public class Report extends TestInfra {
 	private Map<String, String> rstConsumerSummaryData;
 	private Map<String, String> rstReportListData;
 	
-//	@Parameters({ "driver", "browser", "reportsDB" })
-//
-//	@BeforeClass
-//	public void beforeTest(String drivers, String browsers, String reportsDB) {
-//		try {
-//			browser.launch(drivers, browsers);
-//			dataSourceManager.switchToReportsDB(reportsDB);
-//			browser.close();
-//		} catch (Exception exc) {
-//			TestInfra.failWithScreenShot(exc.toString());
-//		}
-//	}
+	@Parameters({ "driver", "browser", "reportsDB" })
+
+	@BeforeClass
+	public void beforeTest(String drivers, String browsers, String reportsDB) {
+		try {
+			browser.launch(drivers, browsers);
+			dataSourceManager.switchToReportsDB(reportsDB);
+			browser.close();
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
+	}
 
 	@Test(description = "119928-This test validates account adjustment report")
 	public void accountAdjustmentReport() {
