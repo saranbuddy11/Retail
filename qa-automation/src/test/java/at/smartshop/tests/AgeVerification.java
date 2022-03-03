71bfb9670ada5ac38950a59687d824d7b0e545c3
Karthik ragavan <karthikr@nousinfo.com>
2022-03-03 11:44:57 +0530

    Create AgeVerification.java

---
+++ qa-automation/src/test/java/at/smartshop/tests/AgeVerification.java
@@ -0,0 +1,9 @@
+package at.smartshop.tests;
+
+import org.testng.annotations.Listeners;
+
+@Listeners(at.framework.reportsetup.Listeners.class)
+
+public class AgeVerification extends TestInfra {
+
+}
