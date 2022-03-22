4b2a85cb5434a255ad94a82f5a6c1bcaee6bee93
ravindhara <ravindhara@nousinfo.com>
2022-03-21 21:15:01 -0700

    Changes made in Transaction canned report
    Changes made in Transaction canned report

--- qa-automation/src/test/java/at/smartshop/tests/Report.java
+++ qa-automation/src/test/java/at/smartshop/tests/Report.java
@@ -148,17 +148,17 @@ public class Report extends TestInfra {
 
 
 
-//	@Parameters({ "driver", "browser", "reportsDB" })
-//	@BeforeClass
-//	public void beforeTest(String drivers, String browsers, String reportsDB) {
-//		try {
-//			browser.launch(drivers, browsers);
-//			dataSourceManager.switchToReportsDB(reportsDB);
-//			browser.close();
-//		} catch (Exception exc) {
-//			TestInfra.failWithScreenShot(exc.toString());
-//		}
-//	}
+	@Parameters({ "driver", "browser", "reportsDB" })
+	@BeforeClass
+	public void beforeTest(String drivers, String browsers, String reportsDB) {
+		try {
+			browser.launch(drivers, browsers);
+			dataSourceManager.switchToReportsDB(reportsDB);
+			browser.close();
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
+	}
 
 	@Test(description = "119928-This test validates account adjustment report")
 
