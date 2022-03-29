447206ae1c21b0cac76f3de37eaf585ec87ab134
Karthik ragavan <karthikr@nousinfo.com>
2022-03-29 16:43:22 +0530

    Update Location.java

--- qa-automation/src/test/java/at/smartshop/tests/Location.java
+++ qa-automation/src/test/java/at/smartshop/tests/Location.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
