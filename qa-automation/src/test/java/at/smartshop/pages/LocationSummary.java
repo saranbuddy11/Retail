4769ab5fd2a288d0e9d510fbef25e15961246257
PrabhaNgm <91895306+PrabhaNgm@users.noreply.github.com>
2022-04-25 11:26:19 +0530

    completed QAA 109, 118

--- qa-automation/src/test/java/at/smartshop/pages/LocationSummary.java
+++ qa-automation/src/test/java/at/smartshop/pages/LocationSummary.java
@@ -299,6 +299,8 @@ public class LocationSummary extends Factory {
 	public static final By CLEAR_INVENTORY_FILTER = By.xpath("//a[@onclick='clearInventoryFilter()']");
 	public static final By FILTER_HOME_CMMRCIAL = By.id("cmrHomeFilterType");
 	public static final By HOME_CMMRCIAL_NAME = By.xpath("//table[@id='cmrHomeGrid']/tbody/tr/td[@aria-describedby='cmrHomeGrid_name']");
+	public static final By DEVICE_RECORD = By.xpath("//span[@id='deviceDataGrid_table_pager_label']");
+	
 
 	public By objAddTopOffSubsidy(int index) {
 		return By.xpath("(//i[@class='fa fa-plus-circle fa-2x primary-color addBtn'])[" + index + "]");
