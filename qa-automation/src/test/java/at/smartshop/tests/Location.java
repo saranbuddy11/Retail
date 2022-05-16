1f92e83f802c1b9fc5928b5118ea9f943554e5c3
PrabhaNgm <91895306+PrabhaNgm@users.noreply.github.com>
2022-05-16 12:26:50 +0530

    completed

--- qa-automation/src/test/java/at/smartshop/tests/Location.java
+++ qa-automation/src/test/java/at/smartshop/tests/Location.java
@@ -1,5 +1,8 @@
 package at.smartshop.tests;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -34,6 +37,7 @@ import at.smartshop.database.columns.CNSuperList;
 import at.smartshop.keys.Configuration;
 import at.smartshop.keys.Constants;
 import at.smartshop.keys.FilePath;
+import at.smartshop.pages.CreatePromotions;
 import at.smartshop.pages.DeviceList;
 import at.smartshop.pages.GlobalProduct;
 import at.smartshop.pages.GlobalProductChange;
@@ -1708,8 +1712,8 @@ public class Location extends TestInfra {
 			foundation.click(LocationSummary.TAB_PRODUCTS);
 			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.BTN_ADD_PRODUCT));
 			foundation.click(LocationSummary.BTN_ADD_PRODUCT);
-			
-			//verifying products UI fields
+
+			// verifying products UI fields
 			foundation.waitforElement(LocationSummary.LBL_ADD_PRODUCT, Constants.SHORT_TIME);
 			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.LBL_ADD_PRODUCT));
 			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.TXT_ADD_PRODUCT_SEARCH));
@@ -1717,18 +1721,18 @@ public class Location extends TestInfra {
 			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.BTN_SELECTALL));
 			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.BTN_SELECTNONE));
 			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.LBL_POPUP_ADD_PRODUCT_CLOSE));
-			
-			//selecting button select all and verifying all products are highlighted
+
+			// selecting button select all and verifying all products are highlighted
 			foundation.scrollIntoViewElement(LocationSummary.BTN_SELECTALL);
 			if (foundation.isDisplayed(LocationSummary.BTN_SELECTALL)) {
 				foundation.click(LocationSummary.BTN_SELECTALL);
 				foundation.waitforElement(LocationSummary.VALIDATE_HIGHLIGHTED_TEXT, Constants.SHORT_TIME);
 				locationSummary.verifyProductsHighlighted("true");
-			}			
-			//selecting button select none and verifying all products are not highlighted
+			}
+			// selecting button select none and verifying all products are not highlighted
 			foundation.click(LocationSummary.BTN_SELECTNONE);
-			locationSummary.verifyProductsHighlighted("false");			
-			
+			locationSummary.verifyProductsHighlighted("false");
+
 		} catch (Exception exc) {
 			TestInfra.failWithScreenShot(exc.toString());
 		}
@@ -1739,16 +1743,16 @@ public class Location extends TestInfra {
 			+ "143219 -QAA-117-ADM>Location Summary>Products>Add Product - Add.")
 	public void verifyAddProductAndCloseButtonFunctionality() {
 		final String CASE_NUM = "177614";
-		
+
 		// Reading test data from DataBas
-		rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);		
+		rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
 		String location = rstLocationData.get(CNLocation.LOCATION_NAME);
 		String product = rstLocationData.get(CNLocation.PRODUCT_NAME);
 		List<String> printGroup = Arrays
 				.asList(rstLocationData.get(CNLocation.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
-		
-		try {			
-			
+
+		try {
+
 			// Select Menu and Menu Item
 			browser.navigateURL(
 					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
@@ -1758,36 +1762,34 @@ public class Location extends TestInfra {
 					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
 
 			locationList.selectLocationName(location);
-			
+
 			// Navigating to products tab
 			foundation.waitforElement(LocationSummary.TAB_PRODUCTS, Constants.SHORT_TIME);
 			foundation.click(LocationSummary.TAB_PRODUCTS);
-			
-			//close the popup by close button
+
+			// close the popup by close button
 			locationSummary.verifyPopUpUIDisplayed();
 			foundation.click(LocationSummary.BTN_CANCEL_PRODUCT);
 			try {
-				foundation.waitforElementToDisappear(LocationSummary.LBL_ADD_PRODUCT,Constants.SHORT_TIME);
+				foundation.waitforElementToDisappear(LocationSummary.LBL_ADD_PRODUCT, Constants.SHORT_TIME);
 				CustomisedAssert.assertFalse(foundation.isDisplayed(LocationSummary.LBL_ADD_PRODUCT));
-			}
-			catch (Exception exc) {
+			} catch (Exception exc) {
 				TestInfra.failWithScreenShot(exc.toString());
 			}
 			locationSummary.verifyProductsUI();
-			
-			//close the popup by 'X' button
+
+			// close the popup by 'X' button
 			locationSummary.verifyPopUpUIDisplayed();
 			foundation.click(LocationSummary.LBL_POPUP_ADD_PRODUCT_CLOSE);
 			try {
-				foundation.waitforElementToDisappear(LocationSummary.LBL_ADD_PRODUCT,Constants.SHORT_TIME);
+				foundation.waitforElementToDisappear(LocationSummary.LBL_ADD_PRODUCT, Constants.SHORT_TIME);
 				CustomisedAssert.assertFalse(foundation.isDisplayed(LocationSummary.LBL_ADD_PRODUCT));
-			}
-			catch (Exception exc) {
+			} catch (Exception exc) {
 				TestInfra.failWithScreenShot(exc.toString());
 			}
 			locationSummary.verifyProductsUI();
-			
-			//adding product and verifying on Product UI
+
+			// adding product and verifying on Product UI
 			foundation.click(LocationSummary.BTN_ADD_PRODUCT);
 			foundation.waitforElementToBeVisible(LocationSummary.TXT_ADD_PRODUCT_SEARCH, Constants.SHORT_TIME);
 			foundation.threadWait(Constants.SHORT_TIME);
@@ -1805,8 +1807,8 @@ public class Location extends TestInfra {
 			textBox.enterText(LocationSummary.TXT_PRODUCT_FILTER, product);
 			foundation.WaitForAjax(7000);
 			Assert.assertTrue(foundation.getText(LocationSummary.PRODUCT_NAME).equals(product));
-			
-			//selecting print group
+
+			// selecting print group
 			locationSummary.selectPrintGroup(product, printGroup.get(0));
 			foundation.click(LocationSummary.TXT_PRODUCT_FILTER);
 			foundation.refreshPage();
@@ -1824,9 +1826,129 @@ public class Location extends TestInfra {
 		} catch (Exception exc) {
 			TestInfra.failWithScreenShot(exc.toString());
 		}
-		//reset the data
+		// reset the data
 		foundation.click(LocationSummary.PRODUCT_NAME);
 		foundation.waitforElement(LocationSummary.BTN_REMOVE, Constants.SHORT_TIME);
 		foundation.click(LocationSummary.BTN_REMOVE);
+	}
+
+	@Test(description = "178509-QAA-122-ADM>Location Summary>Products>Update Min. Stock.")
+	public void UpdateMinimumStock() {
+		final String CASE_NUM = "178509";
+
+		// Reading test data from DataBas
+		rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
+		String location = rstLocationData.get(CNLocation.LOCATION_NAME);
+		String product = rstLocationData.get(CNLocation.PRODUCT_NAME);
+		List<String> minStock = Arrays
+				.asList(rstLocationData.get(CNLocation.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+		try {
+
+			// Select Menu and Menu Item
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+
+			locationList.selectLocationName(location);
+
+			// Navigating to products tab
+			foundation.waitforElement(LocationSummary.TAB_PRODUCTS, Constants.SHORT_TIME);
+			foundation.click(LocationSummary.TAB_PRODUCTS);
+			foundation.WaitForAjax(5000);
+			foundation.waitforElement(LocationSummary.TXT_PRODUCT_FILTER, Constants.SHORT_TIME);
+			foundation.threadWait(Constants.MEDIUM_TIME);
+
+			// updating Min Stock
+			textBox.enterText(LocationSummary.TXT_PRODUCT_FILTER, product);
+			foundation.threadWait(Constants.MEDIUM_TIME);
+			Assert.assertTrue(foundation.getText(LocationSummary.PRODUCT_NAME).equals(product));
+			locationSummary.enterMinStock(product, minStock.get(0));
+			foundation.click(LocationSummary.TXT_PRODUCT_FILTER);
+			foundation.refreshPage();
+			foundation.waitforElement(LocationSummary.TAB_PRODUCTS, Constants.SHORT_TIME);
+			foundation.click(LocationSummary.TAB_PRODUCTS);
+			foundation.WaitForAjax(5000);
+			foundation.waitforElement(LocationSummary.TXT_PRODUCT_FILTER, Constants.SHORT_TIME);
+			foundation.threadWait(Constants.MEDIUM_TIME);
+			textBox.enterText(LocationSummary.TXT_PRODUCT_FILTER, product);
+			foundation.WaitForAjax(7000);
+			Assert.assertTrue(foundation.getText(LocationSummary.MIN_STOCK).equals(minStock.get(0)));
+			locationSummary.enterMinStock(product, minStock.get(1));
+			foundation.click(LocationSummary.TXT_PRODUCT_FILTER);
+
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
+	}
+
+	@Test(description = "178530-QAA-114-ADM>LocationSummary>Verify all the selected products should be highlightedQAA-179-ADM>Location Summary>Promotions>Promotions UI and Fields."
+			+ "178577-QAA-181-ADM>Location Summary>Promotions>Manage Columns."
+			+ "178542-QAA-180-ADM>Location Summary>Promotions>Create Promotion.")
+	public void verifyUIForPromotionsTab() {
+		try {
+			final String CASE_NUM = "178530";
+
+			browser.navigateURL(
+					propertyFile.readPropertyFile(Configuration.CURRENT_URL, FilePath.PROPERTY_CONFIG_FILE));
+			login.login(propertyFile.readPropertyFile(Configuration.CURRENT_USER, FilePath.PROPERTY_CONFIG_FILE),
+					propertyFile.readPropertyFile(Configuration.CURRENT_PASSWORD, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Reading test data from DataBase
+
+			rstLocationData = dataBase.getLocationData(Queries.LOCATION, CASE_NUM);
+			List<String> requiredData = Arrays
+					.asList(rstLocationData.get(CNLocation.REQUIRED_DATA).split(Constants.DELIMITER_TILD));
+
+			String location = rstLocationData.get(CNLocation.LOCATION_NAME);
+
+			// Select Menu and Menu Item
+			navigationBar.selectOrganization(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+
+			locationList.selectLocationName(location);
+
+			// Navigating to promotions tab
+			foundation.waitforElement(LocationSummary.TAB_PROMOTIONS, Constants.SHORT_TIME);
+			foundation.click(LocationSummary.TAB_PROMOTIONS);
+
+			// verifying Promotions UI and fields
+			foundation.waitforElement(LocationSummary.BTN_CREATE_PROMO, Constants.SHORT_TIME);
+			foundation.scrollIntoViewElement(LocationSummary.BTN_CREATE_PROMO);
+			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.BTN_CREATE_PROMO));
+			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.BTN_MANAGE_COLUMNS));
+			CustomisedAssert.assertTrue(foundation.isDisplayed(LocationSummary.PROMOTIONS_SEARCH));
+			locationSummary.verifyPromotionsTableHeaders(requiredData);
+
+			// click on create Promotion button
+			foundation.click(LocationSummary.BTN_CREATE_PROMO);
+			foundation.threadWait(Constants.TWO_SECOND);
+			
+			// validate UI of enter promotion basics page
+			assertTrue(foundation.isDisplayed(CreatePromotions.DPD_PROMO_TYPE));
+			assertTrue(foundation.isDisplayed(CreatePromotions.TXT_PROMO_NAME));
+			assertTrue(foundation.isDisplayed(CreatePromotions.BTN_NEXT));
+			assertTrue(foundation.isDisplayed(CreatePromotions.BTN_CANCEL));
+			assertEquals(foundation.getText(CreatePromotions.LBL_BASICINFO), requiredData.get(9));
+			assertEquals(foundation.getText(CreatePromotions.LBL_ENTER_BASICINFO), requiredData.get(10));
+			foundation.navigateToBackPage();
+			foundation.waitforElement(LocationSummary.TAB_PROMOTIONS, Constants.SHORT_TIME);
+			foundation.click(LocationSummary.TAB_PROMOTIONS);
+			
+			//Validate the Manage column Popup
+			foundation.click(LocationSummary.BTN_MANAGE_COLUMNS);
+			foundation.threadWait(Constants.TWO_SECOND);
+			assertTrue(foundation.isDisplayed(LocationSummary.MANAGE_COLUMN_POPUP_HEADER));
+			assertTrue(foundation.isDisplayed(LocationSummary.MANAGE_COLUMN_RESET_BUTTON));
+			locationSummary.verifyPromotionsTableHeaders(requiredData);
+			assertTrue(foundation.isDisplayed(LocationSummary.MANAGE_COLUMN_APPLY_BUTTON));
+			assertTrue(foundation.isDisplayed(LocationSummary.MANAGE_COLUMN_CANCEL_BUTTON));
+			foundation.click(LocationSummary.MANAGE_COLUMN_CANCEL_BUTTON);
+
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
 	}
 }
