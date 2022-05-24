1f18cac80353f80657cffeebc896858c5d47f865
santosh Chikkala <santoshck@nousinfo.com>
2022-05-24 19:48:07 +0530

    updated

--- qa-automation/src/test/java/at/smartshop/tests/Location.java
+++ qa-automation/src/test/java/at/smartshop/tests/Location.java
@@ -344,7 +344,7 @@ public class Location extends TestInfra {
 			String product = rstGlobalProductChangeData.get(CNGlobalProductChange.PRODUCT_NAME);
 			textBox.enterText(GlobalProductChange.TXT_PRODUCT_NAME, product);
 			foundation.click(GlobalProductChange.BTN_PRODUCT_APPLY);
-			foundation.threadWait(Constants.TWO_SECOND);
+			foundation.threadWait(Constants.SHORT_TIME);
 			foundation.click(globalProductChange.objProductName(product));
 			foundation.waitforClikableElement(GlobalProductChange.BTN_NEXT, Constants.SHORT_TIME);
 			foundation.click(GlobalProductChange.BTN_NEXT);
