5d7110ae36e1eced1e86d603640f644866f39d27
ravindhara <ravindhara@nousinfo.com>
2022-05-24 08:59:05 +0530

    Cross Org: Rate Report data validation (186509)
     Cross Org: Rate Report data validation (186509)

--- qa-automation/src/test/java/at/smartshop/pages/CrossOrgRateReport.java
+++ qa-automation/src/test/java/at/smartshop/pages/CrossOrgRateReport.java
@@ -1,21 +1,36 @@
 package at.smartshop.pages;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import com.aventstack.extentreports.Status;
 
+import at.framework.browser.Factory;
 import at.framework.reportsetup.ExtFactory;
 import at.framework.ui.Foundation;
 import at.smartshop.keys.Constants;
 import at.smartshop.tests.TestInfra;
 
-public class CrossOrgRateReport {
+public class CrossOrgRateReport extends Factory {
 	private Foundation foundation = new Foundation();
 
 	public static final By LBL_REPORT_NAME = By.cssSelector("#report-container > div > div.col-12.comment-table-heading");
 	private static final By REPORT_GRID_FIRST_ROW = By.cssSelector("#rptdt > tbody > tr:nth-child(1)");
 	private static final By NO_DATA_AVAILABLE_IN_TABLE = By.xpath("//td[@class='dataTables_empty']");
+	
+	private static final By TBL_CROSS_ORG_RATE = By.id("rptdt");
+	private static final By TBL_CROSS_ORG_RATE_GRID = By.cssSelector("#rptdt > tbody");
+	public final By SEARCH_RESULT = By.xpath("//input[@aria-controls='rptdt']");
+
+	private List<String> tableHeaders = new ArrayList<>();
+	private Map<Integer, Map<String, String>> reportsData = new LinkedHashMap<>();
 
 	public void verifyReportName(String reportName) {
 		try {
@@ -45,4 +60,70 @@ public class CrossOrgRateReport {
 			TestInfra.failWithScreenShot(exc.toString());
 		}
 	}
+	/**
+	 * This method is to read the data from the Report Table
+	 * @return
+	 */
+	public Map<Integer, Map<String, String>> getTblRecordsUI() {
+		try {
+			int recordCount = 0;
+			tableHeaders.clear();
+			WebElement tableReportsList = getDriver().findElement(TBL_CROSS_ORG_RATE_GRID);
+			WebElement tableReports = getDriver().findElement(TBL_CROSS_ORG_RATE);
+			List<WebElement> columnHeaders = tableReports.findElements(By.cssSelector("thead > tr > th"));
+			List<WebElement> rows = tableReportsList.findElements(By.tagName("tr"));
+			for (WebElement columnHeader : columnHeaders) {
+				tableHeaders.add(columnHeader.getText());
+			}
+			for (WebElement row : rows) {
+				Map<String, String> uiTblRowValues = new LinkedHashMap<>();
+				for (int columnCount = 1; columnCount < tableHeaders.size() + 1; columnCount++) {
+					WebElement column = row.findElement(By.cssSelector("td:nth-child(" + columnCount + ")"));
+					uiTblRowValues.put(tableHeaders.get(columnCount - 1), column.getText());
+				}
+				reportsData.put(recordCount, uiTblRowValues);
+				recordCount++;
+			}
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
+		System.out.println("reportsData : "+ reportsData);
+		return reportsData;
+	}
+	
+    /**
+     * This method is to validate the Report Table Headers
+     * @param columnNames
+     */
+	public void verifyReportHeaders(String columnNames) {
+		try {
+			List<String> columnName = Arrays.asList(columnNames.split(Constants.DELIMITER_HASH));
+			System.out.println("tableHeaders : "+ tableHeaders);
+			System.out.println("columnName : "+ columnName);
+			for (int iter = 0; iter < tableHeaders.size(); iter++) {
+				Assert.assertTrue(tableHeaders.get(iter).equals(columnName.get(iter)));
+			}
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
+	}
+
+	/**
+	 * This method is to validate the Report Data
+	 * @param expextedData
+	 */
+	public void verifyReportData(String expextedData) {
+		try {
+			List<String> expextedDataList = Arrays.asList(expextedData.split(Constants.DELIMITER_HASH));
+			System.out.println("reportsData : "+ reportsData);
+			System.out.println("expextedDataList : "+ expextedDataList);
+				for (int iter = 0; iter < tableHeaders.size(); iter++) {
+					Assert.assertTrue(reportsData.get(0).get(tableHeaders.get(iter))
+							.contains(expextedDataList.get(iter)));
+			}
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
+	}
+
 }
