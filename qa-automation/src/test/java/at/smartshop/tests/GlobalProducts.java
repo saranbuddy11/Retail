fcfec95d9988b35d77df38b7aa47ca0028dfe1ad
Afroseaathika <afrosean@nousinfo.com>
2022-06-07 10:19:01 +0530

    Update GlobalProducts.java

--- qa-automation/src/test/java/at/smartshop/tests/GlobalProducts.java
+++ qa-automation/src/test/java/at/smartshop/tests/GlobalProducts.java
@@ -579,33 +579,33 @@ public class GlobalProducts extends TestInfra {
 			navigationBar.selectOrganization(
 					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
 			navigationBar.navigateToMenuItem(rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM));
-			
-            //create a product
+
+			// create a product
 			foundation.click(GlobalProduct.BTN_CREATE);
 			textBox.enterText(GlobalProduct.TXT_PRODUCTNAME, strings.getRandomCharacter());
 			textBox.enterText(GlobalProduct.TXT_PRICE, String.valueOf(numbers.generateRandomNumber(0, 9)));
 			textBox.enterText(GlobalProduct.LBL_COST, String.valueOf(numbers.generateRandomNumber(0, 9)));
 			foundation.click(GlobalProduct.BUTTON_SAVE);
-			foundation.waitforElement(GlobalProduct.POPUP_HEADER, Constants.SHORT_TIME);			
+			foundation.waitforElement(GlobalProduct.POPUP_HEADER, Constants.SHORT_TIME);
 			foundation.click(GlobalProduct.SAVE_POPUP_BTN);
 			foundation.waitforElement(GlobalProduct.LBL_SCANCODE_MSG, Constants.SHORT_TIME);
 			String actualData = foundation.getText(GlobalProduct.LBL_SCANCODE_ERROR);
 			CustomisedAssert.assertEquals(actualData, expectedError.get(0));
-			
-			//Verify Scancode required 
+
+			// Verify Scancode required
 			foundation.click(GlobalProduct.CANCEL_BTN);
 			foundation.click(GlobalProduct.BTN_CREATE);
 			textBox.enterText(GlobalProduct.TXT_PRODUCTNAME, strings.getRandomCharacter());
 			textBox.enterText(GlobalProduct.TXT_PRICE, String.valueOf(numbers.generateRandomNumber(0, 9)));
 			textBox.enterText(GlobalProduct.LBL_COST, String.valueOf(numbers.generateRandomNumber(0, 9)));
 			foundation.click(GlobalProduct.BUTTON_SAVE);
-			foundation.waitforElement(GlobalProduct.POPUP_HEADER, Constants.SHORT_TIME);			
+			foundation.waitforElement(GlobalProduct.POPUP_HEADER, Constants.SHORT_TIME);
 			foundation.click(GlobalProduct.SAVE_POPUP_BTN);
 			foundation.waitforElement(GlobalProduct.LBL_SCANCODE_MSG, Constants.SHORT_TIME);
 			foundation.click(GlobalProduct.SCANCODE);
 			actualData = foundation.getText(GlobalProduct.LBL_SCANCODE_MSG);
 			CustomisedAssert.assertEquals(actualData, expectedError.get(1));
-			
+
 		} catch (Exception exc) {
 			TestInfra.failWithScreenShot(exc.toString());
 		}
@@ -1140,7 +1140,6 @@ public class GlobalProducts extends TestInfra {
 				rstGlobalProductChangeData.get(CNGlobalProductChange.INCREMENT_PRICE).split(Constants.DELIMITER_TILD));
 		List<String> product = Arrays.asList(
 				rstGlobalProductChangeData.get(CNGlobalProductChange.PRODUCT_NAME).split(Constants.DELIMITER_TILD));
-				
 
 		try {
 			// Select Menu Item & verify the select in Global Product Change for Location(s)
@@ -1450,12 +1449,11 @@ public class GlobalProducts extends TestInfra {
 			foundation.click(GlobalProductChange.BTN_NEXT);
 			globalProductChange.productFieldChange(resetdata);
 			foundation.click(GlobalProductChange.BTN_SUBMIT);
-			foundation.threadWait(Constants.THREE_SECOND);
+			foundation.threadWait(Constants.SHORT_TIME);
 			foundation.click(GlobalProductChange.BTN_OK);
-			foundation.threadWait(Constants.THREE_SECOND);
+			foundation.threadWait(Constants.SHORT_TIME);
 			foundation.click(GlobalProductChange.REASON_BTNOK);
 
 		}
 	}
-	
 }
