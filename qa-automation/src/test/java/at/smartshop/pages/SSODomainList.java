c1dbd646d77121a78ce5dde37ce8edb671587cad
Afroseaathika <afrosean@nousinfo.com>
2022-09-21 09:42:38 +0530

    pushes for SSOdomain

---
+++ qa-automation/src/test/java/at/smartshop/pages/SSODomainList.java
@@ -0,0 +1,58 @@
+package at.smartshop.pages;
+
+import org.openqa.selenium.By;
+
+import at.framework.browser.Factory;
+import at.framework.generic.CustomisedAssert;
+import at.framework.ui.CheckBox;
+import at.framework.ui.Dropdown;
+import at.framework.ui.Foundation;
+import at.framework.ui.TextBox;
+import at.smartshop.keys.Constants;
+
+public class SSODomainList extends Factory {
+
+	private Foundation foundation = new Foundation();
+	private Dropdown dropDown = new Dropdown();
+	private TextBox textBox = new TextBox();
+	private CheckBox checkBox = new CheckBox();
+	private NavigationBar navigationBar = new NavigationBar();
+
+	public static final By LBL_SSO_DOMAIN = By.id("page-title");
+	public static final By BTN_CREATE_NEW = By.id("createNewBtn");
+	public static final By RECORDS_FILTER = By.id("ssodomaingrid_editor_dropDownButton");
+	public static final By TXT_SEARCH = By.id("search-box");
+	public static final By DOMAIN_NAME_GRID = By.id("ssodomaingrid_name");
+	public static final By DOMAIN_ADDRESS_GRID = By.id("ssodomaingrid_address");
+	public static final By CREATE_SSO_DOMAIN = By.xpath("//li[@class='active']");
+	public static final By BTN_CANCEL = By.id("cancelBtn");
+
+	/**
+	 * verify all fields in sso domain list page
+	 */
+	public void verifyAllFieldsInSSODomainListpage() {
+		foundation.waitforElementToBeVisible(LBL_SSO_DOMAIN, Constants.THREE_SECOND);
+		CustomisedAssert.assertTrue(foundation.isDisplayed(BTN_CREATE_NEW));
+		CustomisedAssert.assertTrue(foundation.isDisplayed(RECORDS_FILTER));
+		foundation.waitforElementToBeVisible(TXT_SEARCH, Constants.THREE_SECOND);
+		CustomisedAssert.assertTrue(foundation.isDisplayed(TXT_SEARCH));
+		CustomisedAssert.assertTrue(foundation.isDisplayed(DOMAIN_ADDRESS_GRID));
+		CustomisedAssert.assertTrue(foundation.isDisplayed(DOMAIN_NAME_GRID));
+
+	}
+
+	/**
+	 * Navigate to create SSO Domain and click on cancel
+	 */
+	public void navigateoCreateSSOAndClickOnCancel() {
+		foundation.waitforElementToBeVisible(BTN_CREATE_NEW, Constants.THREE_SECOND);
+		foundation.click(BTN_CREATE_NEW);
+		CustomisedAssert.assertTrue(foundation.isDisplayed(CREATE_SSO_DOMAIN));
+		foundation.waitforElementToBeVisible(BTN_CANCEL, Constants.TWO_SECOND);
+		foundation.click(BTN_CANCEL);
+		foundation.waitforElementToBeVisible(LBL_SSO_DOMAIN, Constants.THREE_SECOND);
+		CustomisedAssert.assertTrue(foundation.isDisplayed(LBL_SSO_DOMAIN));
+
+	}
+
+}
---
+++ qa-automation/src/test/java/at/smartshop/tests/SSODomain.java
@@ -0,0 +1,60 @@
+package at.smartshop.tests;
+
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import at.framework.database.mssql.Queries;
+import at.framework.database.mssql.ResultSets;
+import at.framework.generic.CustomisedAssert;
+import at.framework.ui.Foundation;
+import at.framework.ui.TextBox;
+import at.smartshop.database.columns.CNNavigationMenu;
+import at.smartshop.keys.Configuration;
+import at.smartshop.keys.FilePath;
+import at.smartshop.pages.NavigationBar;
+import at.smartshop.pages.SSODomainList;
+
+@Listeners(at.framework.reportsetup.Listeners.class)
+public class SSODomain extends TestInfra {
+
+	private Foundation foundation = new Foundation();
+	private NavigationBar navigationBar = new NavigationBar();
+	private TextBox textBox = new TextBox();
+	private ResultSets dataBase = new ResultSets();
+	private SSODomainList ssoDomainList= new SSODomainList();
+
+	private Map<String, String> rstNavigationMenuData;
+
+	@Test(description = "198529-Verify display of SSO Domains Option in Super Dropdown")
+	public void verifySSODomain() {
+		final String CASE_NUM = "198529";
+
+		// Reading test data from DataBase
+		rstNavigationMenuData = dataBase.getNavigationMenuData(Queries.NAVIGATION_MENU, CASE_NUM);
+
+		try {
+
+			// Launch ADM as super
+			navigationBar.launchBrowserAsSuperAndSelectOrg(
+					propertyFile.readPropertyFile(Configuration.CURRENT_ORG, FilePath.PROPERTY_CONFIG_FILE));
+
+			// Navigate to Super tab and verify SSO Domain Sub Tab is present or not
+			List<String> tabNames = navigationBar.getSubTabs(rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM));
+			CustomisedAssert.assertTrue(tabNames.contains(rstNavigationMenuData.get(CNNavigationMenu.REQUIRED_OPTION)));
+			
+			//Navigate to SSO Domain page and verify all fields
+			navigationBar.navigateToMenuItem(rstNavigationMenuData.get(CNNavigationMenu.MENU_ITEM));
+			ssoDomainList.verifyAllFieldsInSSODomainListpage();
+			
+			//Navigate to create SSO Domain page and click on cancel button
+			ssoDomainList.navigateoCreateSSOAndClickOnCancel();
+
+		} catch (Exception exc) {
+			TestInfra.failWithScreenShot(exc.toString());
+		}
+
+	}
+}
