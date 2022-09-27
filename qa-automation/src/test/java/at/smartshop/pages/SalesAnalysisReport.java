b994615ed4d291f4d52cb72a2e7cf8c0312be777
Karthik-Nous <karthikr@nousinfo.com>
2022-09-27 16:02:39 +0530

    updated

--- qa-automation/src/test/java/at/smartshop/pages/SalesAnalysisReport.java
+++ qa-automation/src/test/java/at/smartshop/pages/SalesAnalysisReport.java
@@ -284,13 +284,10 @@ public class SalesAnalysisReport extends Factory {
 		try {
 			int count = intialData.size();
 			foundation.threadWait(Constants.TWO_SECOND);
-			System.out.println("reportsData : "+ reportsData);
-			System.out.println("intialData : "+ intialData);
 			for (int counter = 0; counter < count; counter++) {
 				for (int iter = 0; iter < tableHeaders.size(); iter++) {
 					CustomisedAssert.assertTrue(reportsData.get(counter).get(tableHeaders.get(iter))
 							.contains(intialData.get(counter).get(tableHeaders.get(iter))));
-					System.out.println(iter);
 				}
 			}
 		} catch (Exception exc) {
--- qa-automation/src/test/java/at/smartshop/pages/SoldDetails.java
+++ qa-automation/src/test/java/at/smartshop/pages/SoldDetails.java
@@ -51,7 +51,7 @@ public class SoldDetails extends Factory {
 	public final static By TXT_SEARCH_TRANSACTION = By.xpath("//input[@aria-controls='transdt']");
 	public final static By TXT_ID_TRANSACTION = By.cssSelector("#Row_0");
 	public final static By FIND_TRANSACTION = By.xpath("//button[@id='findBtn']");
-	public final static By TXT_SEARCH_FILTER = By.id("filterType");
+	public final static By TXT_SEARCH_FILTER = By.cssSelector("input[aria-controls='rptdt']");
 
 	private List<String> tableHeaders = new ArrayList<>();
 	private Map<String, Object> jsonData = new HashMap<>();
@@ -291,7 +291,8 @@ public class SoldDetails extends Factory {
 		try {
 			for (int i = 0; i < reportsData.size(); i++) {
 				innerMap = reportsData.get(i);
-				innerValue = innerMap.get(columnName);
+				innerValue = innerMap.get(columnName).replace("%", "");
+				System.out.println(value + "-" + innerValue);
 				CustomisedAssert.assertTrue(value.contains(innerValue));
 			}
 		} catch (Exception exc) {
@@ -300,7 +301,7 @@ public class SoldDetails extends Factory {
 	}
 
 	/**
-	 * Calcualte Margin value
+	 * Calculate Margin value
 	 * 
 	 * @param cost
 	 * @param totalPrice
