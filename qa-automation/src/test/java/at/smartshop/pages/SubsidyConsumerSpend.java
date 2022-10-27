045e7d9cfa44d1a6d1143ea272cdb8c899b6334d
Karthik-Nous <karthikr@nousinfo.com>
2022-10-27 10:57:19 +0530

    updated

--- qa-automation/src/test/java/at/smartshop/pages/SubsidyConsumerSpend.java
+++ qa-automation/src/test/java/at/smartshop/pages/SubsidyConsumerSpend.java
@@ -302,9 +302,9 @@ public class SubsidyConsumerSpend extends Factory {
 		foundation.click(ProductSearch.BTN_PRODUCT);
 		String price = foundation.getText(ProductSearch.PRODUCT_PRICE);
 		CustomisedAssert.assertTrue(foundation.isDisplayed(order.objText(orderPage)));
-		foundation.click(Payments.EMAIL_ACCOUNT);
-		foundation.waitforElement(Payments.BTN_EMAIL_LOGIN, Constants.ONE_SECOND);
-		foundation.click(Payments.BTN_EMAIL_LOGIN);
+		foundation.click(Payments.EMAIL_ACCOUNT_BTN);
+//		foundation.waitforElement(Payments.BTN_EMAIL_LOGIN, Constants.ONE_SECOND);
+//		foundation.click(Payments.BTN_EMAIL_LOGIN);
 		foundation.waitforElement(Payments.EMAIL_LOGIN_TXT, Constants.ONE_SECOND);
 		foundation.click(Payments.EMAIL_LOGIN_TXT);
 		foundation.threadWait(Constants.ONE_SECOND);
