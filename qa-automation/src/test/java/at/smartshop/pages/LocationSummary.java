3a333352d9ec851e980e09a70d50befd9c2b24c8
Afroseaathika <afrosean@nousinfo.com>
2022-12-07 13:42:01 +0530

    pushes for failures

--- qa-automation/src/test/java/at/smartshop/pages/LocationSummary.java
+++ qa-automation/src/test/java/at/smartshop/pages/LocationSummary.java
@@ -492,11 +492,11 @@ public class LocationSummary extends Factory {
 	}
 
 	public By objectTopOffCalendarMonthAutomationLocation1(String month) {
-		return By.xpath("/html/body/div[10]/div[1]/table/thead/tr[1]/th[contains(text(),'" + month + "')]");
+		return By.xpath("/html/body/div[5]/div[1]/table/thead/tr[1]/th[contains(text(),'" + month + "')]");
 	}
 
 	public By objectTopOffCalendarDayAutomationLocation1(String day) {
-		return By.xpath("/html/body/div[10]/div[1]/table/tbody/tr/td[text()='" + day + "' and @class=\"day  active\"]");
+		return By.xpath("/html/body/div[5]/div[1]/table/tbody/tr/td[text()='" + day + "' and @class=\"day  active\"]");
 	}
 
 	public By objectTopOffCalendarNewDayAutomationLocation1(String day) {
